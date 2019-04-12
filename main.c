7b338e3ece987e178201554f2c8b905fc0f91075
GeorgiaCasumaro <49173964+GeorgiaCasumaro@users.noreply.github.com>
2019-04-12 15:12:39 +1000

    Update main.c

--- main.c
+++ main.c
@@ -1,42 +1,97 @@
 #include <stdio.h>
+#include <ctype.h>
 
-void encrotciph(char n[], char enc[], int u, int k);
-void decrotciph(char enc[], int u, int k);
-//void encsubciph(char m[], int j, int kc);
+void encrotciph(char str[], int u, int k);
+void decrotciph(char str[], int u, int k);
+void encsubciph(char str[], int u, char alph[]);
 void decsubciph();
 void decencrotciph();
 void decencsubciph();
 
-void encrotciph(char n[], char enc[], int u, int k){
-    for (int g = 0; g < u; g++){
-        if(n[g] == ' '){
-           enc[g] = ' ';
-           continue;
-        //need an if statement for getting the array to stop at the final number 
-        }
-        } 
-  /*      else if(n[g] == '\0') break;
-        if(n[g] < 90){
-            enc[g] = n[g] - 32;
-            continue;   */
-        }
-        n[g] = n[g] - 65;//takes the letter in array g into ASCII code numbers
-        enc[g] = (k + n[g])%(26);//uses the key encrypte 
-        enc[g] = enc[g] + 65;//takes the numbers and turns them back into letters 
-    } 
-     printf("%s\n", enc);//prints the ecnryption
+void encrotciph(char str[], int u, int k){
+    for (int i = 0; i < u; i++){
+        if (isupper(str[i])){
+            str[i] = str[i] - 65;//takes the letter in array g into ASCII code numbers
+            str[i] = (k + str[i]) % 26;//uses the key encrypte 
+            str[i] = str[i] + 65;//takes the numbers and turns them back into letters  
+}     
+} 
+     printf("\n\n%s", str);//prints the ecnryption
+}
+void decrotciph(char str[], int u, int k){
+    for(int i = 0; i < u; i++){
+         if(isupper(str[i])){
+         str[i] = 26 + str[i] - 65;
+         str[i] = (str[i] - k) % 26;
+         str[i] = str[i] + 65;
+}              
+}
+     printf("%s\n", str);
+}
+void encsubciph(char str[], int u, char alph[]){
+    int f;
+    for(int i = 0; i < u; i++){
+         if(isupper(str[i])){
+             f = str[i] - 65;
+             str[i] = alph[f];
+}
+}
+    printf("%s\n", str);
 }
 
-void decrotciph(char enc[], int u, int k){
+
+    
+int main(){
+    int k = 1;  
+    char selec='c';
+    int u;
+    char str[] = "PLEASE PLEASE HELP ME";
+    u = sizeof(str)/sizeof(char);
+    char alph[] = "QWERTYUIOPASDFGHJKLZXCVBNM";
+    printf("%d\n", u); //shows the size of the array 
+    printf("Please select and option: \n");
+    printf("a) Encrypt a rotation cipher \n");
+    printf("b) Decrypt a rotation cipher \n");
+    printf("c) Encrypt a substitution cipher \n");
+    printf("d) Decrypt a substitution cipher \n");
+    printf("e) Decrypt a message encrypted with rotation cipher \n");
+    printf("f) Decrypt a message ecrypted with substitution cipher \n");
+    printf("Selection: \n");
+    //scanf(" %c", &selec); 
+    
+    switch(selec) {
+        case 'a':
+        encrotciph(str, u, k);
+        //break;
+        case 'b': 
+        decrotciph(str, u, k);
+        break;
+        case 'c': 
+        encsubciph(str, u, alph);
+        break;
+ /*       case 'd': decsubciph();
+        break;
+        case 'e': decencrotciph();
+        break;
+        case 'f': decencsubciph();*/
+        default: printf("Unknown option %c\n Please enter a, b, c, d, e, f\n", selec);
+    } 
+    
+    int j,f;
     for(int i = 0; i < u; i++){
-         if(enc[i] == ' ') continue;
-         enc[i] -= 65;
-         if(enc[i] < 0) enc[i] += 26;
-         enc[i] = (enc[i] - k) % 26;
-         enc[i] += 65;
-     }
-     printf("%s\n", enc);
+        if(isupper(str[i])){
+        
+        j = str[i] - 65;
+        alph[f] = j;
+        str[i] = j + 65;              
+    }
+  printf("%s\n", str);
+    
+
 }
+     return 0;
+}
+
 
 int main(){
     int k = 1;
